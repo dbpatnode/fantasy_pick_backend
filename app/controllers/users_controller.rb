d0bf5a3874442cac32f0ecde43ef4d3fa07697b0
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-12-14 11:31:32 +0200

    added uuid to users table, user controller and serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,7 @@ end
     private
   
     def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation, :wins, :losses, :team_id)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :wins, :losses, :team_id, :uuid)
     end
 end
   
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :wins, :losses, :draws, :joined_leagues, :picks
+  attributes :id, :username, :email, :wins, :losses, :draws, :joined_leagues, :picks, :uuid
   
   def picks 
     ActiveModel::SerializableResource.new(object.picks, each_serializer: PickSerializer)
